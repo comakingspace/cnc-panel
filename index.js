50eddeafa842f8d1d243cabc51d5f67696cc4b39
Kim Almasan <kim@almasan.info>
2019-03-29 23:00:45 +0100

    Fix service, rotary, add default settings

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const io = require('socket.io-client')
 const jwt = require('jsonwebtoken')
 const EventEmitter = require('events')
 const gpio = require('rpi-gpio')
-const { DIR_HIGH, EDGE_RISING, MODE_BCM } = require('rpi-gpio')
+const defaultSettings = require('./settings.json')
+
+const { DIR_HIGH, EDGE_RISING, MODE_BCM, EDGE_FALLING, DIR_LOW } = require('rpi-gpio')
 const fs = require('fs')
 
 const offConfig = JSON.parse(fs.readFileSync('/home/pi/.cncrc'))
@@ -16,11 +18,12 @@ class CNCRouter {
         this._distance = 1
         this._distanceIncrement = 10
         this._state = {}
+        this._defaultSettings = Array.from(defaultSettings)
     }
 
     get _token() {
         return this._jwt.sign({ id: '', name: '' }, this._config.secret, {
-            expiresIn: '30d',
+            expiresIn: '365d',
         })
     }
 
@@ -43,25 +46,35 @@ class CNCRouter {
             controllerType: this._config.controllerType,
         })
 
-        socket.on('serialport:open', (options) => {
+        socket.on('serialport:open', (...arg) => {
             // This should contain logic to check the GRBL Settings.
             // It should read a txt file which contains the needed settings and compare them to the current settings
             // If the settings are different, it needs to overwrite the ones currently on the machine with the ones from the txt file
             // There is a python implementation in https://github.com/comakingspace/WorkBee
             // https://github.com/comakingspace/do-something/issues/45 Tracks the discussions around it
+            // console.dir('open', arg)
+            this._send(this._defaultSettings.pop())
         })
 
         socket.on('serialport:error', (options) => {
+            // console.dir('err', options)
         })
 
         socket.on('serialport:read', (data) => {
+            // console.dir('read ', data)
         })
 
         socket.on('serialport:write', (data) => {
+            if (this._defaultSettings.length > 0) {
+                const setting = this._defaultSettings.pop()
+                this._send(setting)
+            }
+            // console.dir('write', data)
         })
 
         socket.on('Grbl:state', (state) => {
             this.state = state
+            // console.log('state', state)
             // We should check, what information is available here
             // If this contains information about the remaining time of the job, this should be published on mqtt. Topic: /CNC/Status
         })
@@ -87,50 +100,62 @@ class CNCRouter {
     }
 
     forward() {
+        console.log('pressed: forward')
         this._send(`G91;\nY-${this._distance};\nG90;\n`)
     }
 
     backward() {
+        console.log('pressed: backward')
         this._send(`G91;\nY${this._distance};\nG90;\n`)
     }
 
     right() {
+        console.log('pressed: right')
         this._send(`G91;\nX${this._distance};\nG90;\n`)
     }
 
     left() {
+        console.log('pressed: left')
         this._send(`G91;\nX-${this._distance};\nG90;\n`)
     }
 
     up() {
+        console.log('pressed: up')
         this._send(`G91;\nZ${this._distance};\nG90;\n`)
     }
 
     down() {
+        console.log('pressed: down')
         this._send(`G91;\nZ-${this._distance};\nG90;\n`)
     }
 
     reset() {
+        console.log('pressed: reset')
         //     this._send(`G91;\nY-${this._distance};\nG90;\n`)
     }
 
     unlock() {
+        console.log('pressed: unlock')
         this._send(`$X;\n`)
     }
 
     pause() {
+        console.log('pressed: paused')
         //     this._send(`G91;\nY-${this._distance};\nG90;\n`)
     }
 
     zeroLeftRight() {
+        console.log('pressed: zeroLeftRight')
         this._send(`G90;\nX0;\n`)
     }
 
     zeroForwardBack() {
+        console.log('pressed: zeroForwardBack')
         this._send(`G90;\nY0;\n`)
     }
 
     zeroUpDown() {
+        console.log('pressed: zeroUpDown')
         this._send(`G90;\nZ0;\n`)
     }
 
@@ -147,6 +172,7 @@ class CNCRouter {
         // 100mm
         // 200mm
         // Do we need some initialization to find the correct value during boot?
+        console.log(dist)
         this.distance = dist
     }
 }
@@ -160,19 +186,33 @@ class Buttons extends EventEmitter {
     }
 
     async _setup() {
-        Array.from(this._btns.keys()).forEach(btn => {
+        Array.from(this._btns.keys()).forEach(btnID => {
+            const currentButton = this._btns.get(btnID);
             this._gpio.setMode(MODE_BCM)
-            this._gpio.setup(btn, DIR_HIGH, EDGE_RISING,
-                (err) => {
-                    this.emit(err ? 'error' : 'success', {
-                        ...err,
-                        btn,
-                    } || btn)
-                })
+            this._gpio.setup(btnID, DIR_HIGH, currentButton.edge || EDGE_RISING, (err) => {
+                if (err) {
+                    this.emit('error', { ...err, btn: btnID })
+                    return
+                }
+
+                if (currentButton.name == 'rotary') {
+                    this._gpio.read(btnID, (e, value) => {
+                        if (e) {
+                            return;
+                        }
+                        if (!value) {
+                            this.emit(currentButton.name, currentButton.parse(value))
+                        }
+                    })
+                }
+                this.emit('success', { id: btnID })
+            })
         })
+
+
         this._gpio.on('change', (channel, value) => {
-            // console.log('buttons work', channel, value)
             const btn = this._btns.get(channel)
+            console.log('buttons work', channel, value, btn)
             this.emit(btn.name, btn.parse(value))
         })
     }
@@ -192,12 +232,12 @@ const buttonMap = new Map([
     [27, { name: 'zeroLeftRight', parse: (value) => !!value }],
     [22, { name: 'zeroForwardBack', parse: (value) => !!value }],
     [10, { name: 'zeroUpDown', parse: (value) => !!value }],
-    [9, { name: 'rotary', parse: () => 0.1 }],
-    [11, { name: 'rotary', parse: () => 0.5 }],
-    [5, { name: 'rotary', parse: () => 1 }],
-    [6, { name: 'rotary', parse: () => 5 }],
-    [19, { name: 'rotary', parse: () => 10 }],
-    [26, { name: 'rotary', parse: () => 50 }],
+    [9, { name: 'rotary', parse: () => 0.1, edge: EDGE_FALLING }],
+    [11, { name: 'rotary', parse: () => 0.5, edge: EDGE_FALLING }],
+    [5, { name: 'rotary', parse: () => 1, edge: EDGE_FALLING }],
+    [6, { name: 'rotary', parse: () => 5, edge: EDGE_FALLING }],
+    [19, { name: 'rotary', parse: () => 10, edge: EDGE_FALLING }],
+    [26, { name: 'rotary', parse: () => 50, edge: EDGE_FALLING }],
 ])
 
 const router = new CNCRouter()
@@ -205,6 +245,9 @@ const buttons = new Buttons(buttonMap)
 
 buttons.on('error', (err) => {
     console.log('btns error', err)
+    process.exit(1)
 })
 
-Array.from(buttonMap.values()).forEach(btn => buttons.on(btn.name, router[btn.name].bind(router)))
+const buttonsToListen = new Set(Array.from(buttonMap.values()).map(({ name }) => name))
+
+Array.from(buttonsToListen).forEach(btnName => buttons.on(btnName, router[btnName].bind(router)))
